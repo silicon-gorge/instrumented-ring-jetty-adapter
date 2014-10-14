61453077acf1bd555e97b24d8efefdea7ff7b18b
Neil Prosser <neil.prosser@gmail.com>
2014-10-14 11:45:20 +0100

    Version 1.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mixradio/instrumented-ring-jetty-adapter "1.0.3-SNAPSHOT"
+(defproject mixradio/instrumented-ring-jetty-adapter "1.0.3"
 
   :description "Instrumented Jetty"
   :url "https://github.com/mixradio/instrumented-ring-jetty-adapter"
