33b172b23bd3533d71eba25b00eb5cc2e96ca88c
Neil Prosser <neil.prosser@gmail.com>
2015-05-18 22:06:13 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,16 +4,16 @@
   :url "https://github.com/mixradio/instrumented-ring-jetty-adapter"
   :license "https://github.com/mixradio/instrumented-ring-jetty-adapter/blob/master/LICENSE"
 
-  :dependencies [[ch.qos.logback/logback-access "1.1.2"]
-                 [com.codahale.metrics/metrics-jetty9 "3.0.2"]
+  :dependencies [[ch.qos.logback/logback-access "1.1.3"]
                  [environ "1.0.0"]
-                 [metrics-clojure "2.3.0"]
-                 [org.eclipse.jetty/jetty-server "9.2.3.v20140905"]
-                 [ring/ring-core "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1" :exclusions [org.eclipse.jetty/jetty-server]]
-                 [ring/ring-servlet "1.3.1"]]
+                 [io.dropwizard.metrics/metrics-jetty9 "3.1.1"]
+                 [metrics-clojure "2.5.1"]
+                 [org.eclipse.jetty/jetty-server "9.2.9.v20150224"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-jetty-adapter "1.3.2" :exclusions [org.eclipse.jetty/jetty-server]]
+                 [ring/ring-servlet "1.3.2"]]
 
   :aot [mixradio.instrumented-jetty]
 
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.6.0"]
-                                       [org.slf4j/slf4j-nop "1.7.7"]]}})
+                                       [org.slf4j/slf4j-nop "1.7.12"]]}})
